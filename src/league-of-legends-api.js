305264f29947f6d470a5eb7d10d8838c0589d7e1
Nick Phillips <imprecatorr@gmail.com>
2018-07-28 22:43:07 -0500

    Add base League of Legends API

--- src/league-of-legends-api.js
+++ src/league-of-legends-api.js
@@ -11,57 +11,134 @@ exports.handler = (event, context) => {
             event.body = JSON.parse(event.body);
         }
     }
-    // const required = ['platform', 'epicNickname'].filter((property) => !event.body[property]);
-    // if(required.length > 0){
-    //     return Promise.reject({
-    //         statusCode: 400,
-    //         message: `Required properties missing: "${required.join('", "')}".`
-    //     });
-    // }
+    const required = ['region', 'summonerName'].filter((property) => !event.body[property]);
+    if(required.length > 0){
+        return Promise.reject({
+            statusCode: 400,
+            message: `Required properties missing: "${required.join('", "')}".`
+        });
+    }
     let promises = [];
-    var options = {
-        url: 'https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/RiotSchmick?api_key=' + process.env.API_KEY,
-        // url: 'https://api.fortnitetracker.com/v1/profile/' + event.body.platform + '/' + event.body.epicNickname,
-        // headers: {
-        //     'TRN-Api-Key': process.env.API_KEY
-        // }
+    let options = {
+        url: 'https://na.api.riotgames.com/lol/summoner/v3/summoners/by-name/' + event.body.summonerName + '?api_key=' + process.env.API_KEY // this gives summoner level basically and accountId, id is summoner id
     };
+    promises.push(request(options).promise().then((res) => {
+        return res;
+    }).catch(function (err) {
+        return undefined;
+    }));
+
+    options = {
+        url: 'https://na1.api.riotgames.com/lol/summoner/v3/summoners/by-name/' + event.body.summonerName + '?api_key=' + process.env.API_KEY // this gives summoner level basically and accountId, id is summoner id
+    };
+    promises.push(request(options).promise().then((res) => {
+        return res;
+    }).catch(function (err) {
+        return undefined;
+    }));
+
+    options = {
+        url: 'https://' + event.body.region + '.api.riotgames.com/lol/summoner/v3/summoners/by-name/' + event.body.summonerName + '?api_key=' + process.env.API_KEY // this gives summoner level basically and accountId, id is summoner id
+    };
+    promises.push(request(options).promise().then((res) => {
+        return res;
+    }).catch(function (err) {
+        return undefined;
+    }));
+
+    options = {
+        url: 'https://ddragon.leagueoflegends.com/realms/na.json'
+    };
+
     promises.push(request(options).promise().then((res) => {
         return res;
     }).catch(function (err) {
         return Promise.reject({
             statusCode: err.statusCode,
-            message: 'Error interacting with Riot Games API.'
+            message: 'Error interacting with DDragon API.'
         });
     }));
 
     return Promise.all(promises).then((responses) => {
-        const[results] = responses;
-        return context.succeed({
-            statusCode: 200,
-            body: JSON.stringify(results),
-            headers: {
-                'Content-Type': 'application/json',
-                'Access-Control-Allow-Methods': 'POST',
-                'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,XAmz-Security-Token',
-                'Access-Control-Allow-Origin': '*'
+        const [na, na1, rest, realms] = responses;
+        let region = '';
+        let results = '';
+        if(event.body.region === 'NA'){
+            if(na !== undefined){
+                region = 'na';
+                results = JSON.parse(na);
+            } else {
+                region = 'na1';
+                results = JSON.parse(na1);
             }
+        } else {
+            region = event.body.region;
+            results = JSON.parse(rest);
+        }
+        
+        let promises = [];
+        let options = {
+            url: 'https://' + region + '.api.riotgames.com/lol/match/v3/matchlists/by-account/' + results.accountId + '?api_key=' + process.env.API_KEY
+        };
+        promises.push(request(options).promise().then((res) => {
+            return res;
+        }).catch(function (err) {
+            return Promise.reject({
+                statusCode: err.statusCode,
+                message: 'Error interacting with Riot Games API.'
+            });
+        }));
+
+        options = {
+            url: 'https://' + region + '.api.riotgames.com/lol/league/v3/positions/by-summoner/' + results.id + '?api_key=' + process.env.API_KEY
+        };
+        promises.push(request(options).promise().then((res) => {
+            return res;
+        }).catch(function (err) {
+            return Promise.reject({
+                statusCode: err.statusCode,
+                message: 'Error interacting with Riot Games API.'
+            });
+        }));
+
+        options = {
+            url: 'http://ddragon.leagueoflegends.com/cdn/' + JSON.parse(realms).n.summoner + '/data/en_US/champion.json'
+        };
+        promises.push(request(options).promise().then((res) => {
+            return res;
+        }).catch(function (err) {
+            return Promise.reject({
+                statusCode: err.statusCode,
+                message: 'Error interacting with Riot Games API.'
+            });
+        }));
+
+        return Promise.all(promises).then((responses) => {
+            let[matchList, positions, allChampions] = responses;
+            let matchesProcessed = JSON.parse(matchList);
+            allChampions = JSON.parse(allChampions);
+            let names = Object.getOwnPropertyNames(allChampions.data);
+            let championMap = new Map();
+            for(var i = 0; i < names.length; i++) {
+                championMap.set(Number(allChampions.data[names[i]].key), allChampions.data[names[i]].name);
+            }
+            for(var j = 0; j < matchesProcessed.matches.length; j++){
+                matchesProcessed.matches[j].championName = championMap.get(matchesProcessed.matches[j].champion);
+            }
+            let returnObject = {};
+            returnObject.account = results;
+            returnObject.matchList = matchesProcessed;
+            returnObject.positions = positions;
+            return context.succeed({
+                statusCode: 200,
+                body: JSON.stringify(returnObject),
+                headers: {
+                    'Content-Type': 'application/json',
+                    'Access-Control-Allow-Methods': 'POST',
+                    'Access-Control-Allow-Headers': 'Content-Type,X-Amz-Date,Authorization,X-Api-Key,XAmz-Security-Token',
+                    'Access-Control-Allow-Origin': '*'
+                }
+            });
         });
     });
 }
-
-
-
-
-// BR	BR1	br1.api.riotgames.com
-// EUNE	EUN1	eun1.api.riotgames.com
-// EUW	EUW1	euw1.api.riotgames.com
-// JP	JP1	jp1.api.riotgames.com
-// KR	KR	kr.api.riotgames.com
-// LAN	LA1	la1.api.riotgames.com
-// LAS	LA2	la2.api.riotgames.com
-// NA	NA1, NA *	na1.api.riotgames.com
-// OCE	OC1	oc1.api.riotgames.com
-// TR	TR1	tr1.api.riotgames.com
-// RU	RU	ru.api.riotgames.com
-// PBE	PBE1	pbe1.api.riotgames.com
